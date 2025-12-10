d1d87a18ebc8281e663c230fc6079bf34ed6d905
nirajkr26 <nirajkumargupta2642006@gmail.com>
2025-12-10 11:07:32 +0530

    auth routes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,43 +1,71 @@
+import "dotenv/config"
 import express from "express"
 import jwt from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config"
 import { middleware } from "./middleware"
-import { CreateUserSchema } from "@repo/common/types"
-
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
+import { prismaClient } from "@repo/db/client"
 const app = express()
 
 app.use(express.json())
 
 app.post("/signup", async (req, res) => {
     try {
-        const data = CreateUserSchema.safeParse(req.body)
+        const parseddata = CreateUserSchema.safeParse(req.body)
 
-        if (!data.success) {
-            return res.json({
-                message: "Incorrect credentials"
+        if (!parseddata.success) {
+            res.json({
+                message: "Incorrect inputs"
             })
+            return;
         }
 
-        res.json({
-            userId: 1
+        const User = await prismaClient.user.create({
+            data: {
+                name: parseddata.data?.name,
+                email: parseddata.data?.username,
+                password: parseddata.data?.password
+            }
         })
 
-    } catch (err) {
+        res.json({
+            userId: User.id
+        })
 
+    } catch (err: any) {
+        res.status(400).json({ message: err.message })
     }
 })
 
 app.post("/signin", async (req, res) => {
     try {
-        const userId = 1;
-        const token = jwt.sign({ userId }, JWT_SECRET)
+        const parsedData = SigninSchema.safeParse(req.body)
+        if (!parsedData.success) {
+            res.json({
+                message: "Incorrect inputs"
+            })
+            return;
+        }
+        const User = await prismaClient.user.findFirst({
+            where: {
+                email: parsedData.data.username
+            }
+        })
 
+        if (User?.password != parsedData.data.password) {
+            res.json({ message: "Incorrect credentials" })
+            return;
+        }
+
+        const userId = User.id;
+        const token = jwt.sign({ userId }, JWT_SECRET)
+        res.setHeader("authorization", token)
         res.json({
             token
         })
 
-    } catch (err) {
-        console.log(err)
+    } catch (err: any) {
+        res.status(400).json({ message: err.message })
     }
 })
 
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,10 @@
+import * as dotenv from  "dotenv"
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg"
+dotenv.config()
 
-export const prismaClient = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+
+export const prismaClient = new PrismaClient({ adapter });
